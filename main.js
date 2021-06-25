214c14e77cba45b452cd91f0667fddcf81e35242
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-06-25 13:17:07 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 
-for (const key of array) { // Цыкл для получение из обьектов ключи или значение.
+for (const key of array) { // Цыкл для получение из обьектов ключи. 
 } 
+
+
+for (const [key,val] of array) {
+  console.log(`${key} : ${val}`)
